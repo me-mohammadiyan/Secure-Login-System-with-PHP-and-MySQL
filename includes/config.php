08bd1ff7aebe0b29e44d82e8a0f7518a0e652fbf
me-mohammadiyan <89594573+me-mohammadiyan@users.noreply.github.com>
2021-08-26 20:16:34 +0430

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -6,3 +6,4 @@ define("HOST", "localhost");
 define("USER", "root"); 			//DATABASE USERNAME
 define("PASSWORD", ""); 	// DATABASE PASSWORD 
 define("DATABASE", "login");   //DATABASE NAME
+define("HTTPS", FALSE); 
