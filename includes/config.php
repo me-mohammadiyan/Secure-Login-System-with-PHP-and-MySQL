4da003e80821e8ed3e7f7b9a0913c019be655d77
me-mohammadiyan <89594573+me-mohammadiyan@users.noreply.github.com>
2021-08-26 20:12:13 +0430

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+
+/* DATABASE LOGIN  */
+define("HOST", "localhost");
+define("USER", "root"); 			//DATABASE USERNAME
+define("PASSWORD", ""); 	// DATABASE PASSWORD 
+define("DATABASE", "login");   //DATABASE NAME
